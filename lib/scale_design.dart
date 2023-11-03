a9ecc0d5544c8f9717108f5fb52ee0bc8cdfd2af
jack <291148484@163.com>
2023-11-03 22:03:10 +0800

    v0.0.3

--- lib/scale_design.dart
+++ lib/scale_design.dart
@@ -1,15 +1,16 @@
 library scale_design;
 
 export './src/size.dart'
-    show Scale, scaleHeight, scaleWidth, perWidth, perHeight;
+    show Scale, scaleHeight, scaleWidth, scaleFont, perWidth, perHeight;
 
 export './src/widgets/text/t.dart' show T;
 export './src/widgets/text/t_span.dart' show TSpan;
 
 export './src/widgets/button/elevated_btn.dart' show ElevatedBtn;
+export './src/widgets/button/floating_action_btn.dart' show FloatingActionBtn;
 export './src/widgets/button/icon_btn.dart' show IconBtn;
-
-// 其它组件
+export './src/widgets/button/outlined_btn.dart' show OutlinedBtn;
+export './src/widgets/button/text_btn.dart' show TextBtn;
 export './src/widgets/button/circular_icon.dart' show CircularIcon;
 export 'src/components/scrollable_Icons_card.dart' show ScrollableIconsCard;
 export './src/components/toast.dart' show Toast;
